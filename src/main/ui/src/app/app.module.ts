28e82aed4778fda282fcfd192f96a7868242f467
adria <adria@adria-VirtualBox>
2023-03-24 14:33:21 +0100

    Refactor Tauula referències
    Veure, ordenar, items per pàgina i filtre (parcial)

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { ReferencesComponent } from './references/references.component';
@@ -17,6 +24,12 @@ import { ReferencesComponent } from './references/references.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main/ui/src/app/references/references.component.ts
+++ src/main/ui/src/app/references/references.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { DataService } from '../data.service';
 import { Reference } from '../dataModels/reference';
 import { Researcher } from '../dataModels/researcher';
 import { ProjectService } from '../project.service';
+import { MatSort, Sort } from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { Exclusion } from '../dataModels/exclusion';
 
 @Component({
   selector: 'app-references',
@@ -14,10 +18,14 @@ export class ReferencesComponent implements OnInit{
   references: Reference[] = []
   projectId: number  = NaN
   sortedData: Reference[] = []
+  dataSource!: MatTableDataSource<Reference>;
+  displayedColumns: string[] = ['doi','ref', 'dl', 'year', 'auth', 'tit', 'ven', 'sta', 'cri', 'inf', 'cla'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
 
   constructor(private dataService: DataService, private projectService: ProjectService) {}
-
-  
 
   ngOnInit(){
     this.getProject();
@@ -41,6 +49,10 @@ export class ReferencesComponent implements OnInit{
       console.log(resposta , 'User resume response');
       this.references = resposta;
       this.sortedData = this.references.slice();
+      this.dataSource = new MatTableDataSource(resposta);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sortData = this.sortData();
+      this.dataSource.sort = this.sort;
     })
   }
 
@@ -52,50 +64,115 @@ export class ReferencesComponent implements OnInit{
     });
   }
   
-  sortData(sort: any) {
-    const data = this.references.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedData = data;
-      return;
+  sortData() {
+    let sortFunction =
+    (refs: Reference[], sort: MatSort): Reference[] => {
+      if (!sort.active || sort.direction === '') {
+        return refs;
+      }
+
+      return refs.sort((a, b) => {
+        const isAsc = sort.direction === 'asc';
+        switch (sort.active) {
+          case 'doi':
+            return compare(a.art.doi, b.art.doi, isAsc);
+          case 'ref':
+            return compare(a.idRef, b.idRef, isAsc);
+          case 'dl':
+            return compare(a.dl.name, b.dl.name, isAsc);
+          case 'year':
+            return compare(a.art.any, b.art.any, isAsc);
+          case 'auth':
+            return compareAuthors(a.art.researchers, b.art.researchers, isAsc);
+          case 'tit':
+            return compare(a.art.title, b.art.title, isAsc);
+          case 'ven':
+            return compare(a.art.ven.name, b.art.ven.name, isAsc);
+          case 'sta':
+            return compare(a.state,b.state, isAsc);
+          case 'cri':
+            return compareCriteria(a.exclusionDTOList, b.exclusionDTOList, isAsc);
+          default:
+            return 0;
+        }
+      });
     }
 
-    this.sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'doi':
-          return compare(a.art.doi, b.art.doi, isAsc);
-        case 'ref':
-          return compare(a.idRef, b.idRef, isAsc);
-        case 'dl':
-          return compare(a.dl.name, b.dl.name, isAsc);
-        case 'year':
-          return compare(a.art.any, b.art.any, isAsc);
-        /*case 'auth':
-          return compare(a.art.researchers, b.art.researchers, isAsc);*/
-        default:
-          return 0;
-      }
-    });
+    return sortFunction; 
+  }
 
-    
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
 
 function compare(a: Number | String, b: Number | String, isAsc: boolean) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    return (a > b ? -1 : 1) * (isAsc ? 1 : -1);
 }
 
-/*function compare(a: Researcher[], b: Researcher[], isAsc: boolean) {
+function compareAuthors(a: Researcher[], b: Researcher[], isAsc: boolean) {
     if (a != null) {
       if (b == null) {
         return -1 * (isAsc ? 1 : -1)
       }
-      else {
-        for (let elem of a) {
-
+      else{
+        var nomsA = "";
+        for(let elem of a) {
+          nomsA += elem.name
         }
+
+        var nomsB = "";
+        for(let elem of b) {
+          nomsB += elem.name
+        }
+
+        return compare(nomsA,nomsB,isAsc)
+      }
+
+    }
+    else {
+      if (b == null) {
+        return -1 * (isAsc ? 1 : -1)
+      }
+      else {
+        return 1 * (isAsc ? 1 : -1)
       }
     }
-}*/
+}
+
+function compareCriteria(a: Exclusion[], b: Exclusion[], isAsc: boolean) {
+  if (a != null) {
+    if (b == null) {
+      return -1 * (isAsc ? 1 : -1)
+    }
+    else{
+      var nomsA = "";
+      for(let elem of a) {
+        nomsA += elem.nameICEC
+      }
+
+      var nomsB = "";
+      for(let elem of b) {
+        nomsB += elem.nameICEC
+      }
+
+      return compare(nomsA,nomsB,isAsc)
+    }
+
+  }
+  else {
+    if (b == null) {
+      return -1 * (isAsc ? 1 : -1)
+    }
+    else {
+      return 1 * (isAsc ? 1 : -1)
+    }
+  }
+}
 
 
